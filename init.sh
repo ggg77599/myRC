6c72604a058f8a475cb52951e4e628f350291d28
MrG <ggg77599@gmail.com>
2016-02-11 19:51:52 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,35 @@
+wget mrgee.me/dl/rc.tar
+tar xvf rc.tar
+mv -f rc/vimrc .vimrc
+mv -f rc/tmux.conf .tmux.conf
+
+rm -rfd rc*
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get dist-upgrade -y
+
+sudo apt-get install python-dev -y
+sudo apt-get install python-pip -y
+
+sudo apt-get install vim -y
+
+sudo apt-get install openssh-server -y
+
+sudo apt-get install git -y
+
+sudo apt-get install gcc-multilib -y 
+sudo apt-get install g++-multilib -y
+
+sudo apt-get install tmux -y
+
+sudo apt-get install nmap -y
+
+
+sudo pip install requests
+sudo pip install virtualenv
+sudo pip install requests
+
+
+
